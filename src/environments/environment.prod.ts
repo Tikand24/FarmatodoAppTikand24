2cc6cc35373c9a03c21dab94d89f1daa05a6fb3f
jonatan_villalobos <jonathan.villalobos@manawac.co.uk>
2021-01-28 16:48:42 -0500

    Variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  MARVEL_API: 'http://gateway.marvel.com/v1/public',
+  MARVEL_AUTHORIZATION_API: 'apikey=134265954254c435994289f9ffb0cb3e&hash=89937d1986bc4d4aadce7ac7a3c69e66&ts=1',
 };
